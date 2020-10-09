748969010def8cc9242faa0a916c9587b3910fce
SAITO Kazuya <saito@smartalgorithm.co.jp>
2020-10-10 01:03:55 +0900

    GORM sample code

--- api/server.go
+++ api/server.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Task struct {
+	gorm.Model
+	Title     string
+	Completed bool `gorm:"default:false"`
+}
 
 func main() {
-	fmt.Println("Hello world")
+	dsn := "host=db user=postgres password=postgres dbname=todo_dev port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&Task{})
+
+	task := Task{Title: "Test"}
+	db.Create(&task)
+	fmt.Println(task)
+
+	db.Model(&task).Updates(map[string]interface{}{"Completed": true})
+	fmt.Println(task)
+
+	db.Delete(&task)
 }

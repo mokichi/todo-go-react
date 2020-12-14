f42419c66e0099f2e3f5c4a1671e4a8eb90da69c
SAITO Kazuya <saito@smartalgorithm.co.jp>
2020-12-15 01:12:20 +0900

    Refactor: use custom hook

--- spa/src/App.tsx
+++ spa/src/App.tsx
@@ -1,62 +1,15 @@
-import { useState, useEffect } from 'react'
+import useTodo from './hooks/useTodo'
 import TaskForm from './components/TaskForm'
 import TodoList from './components/TodoList'
-import axios from 'axios'
 
 export default function App() {
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [completedTasks, setCompletedTasks] = useState<Task[]>([])
-
-  const compareTasks = (a: Task, b: Task) => {
-    return Date.parse(b.updatedAt) - Date.parse(a.updatedAt)
-  }
-  const addTask = async (title: string) => {
-    if (!title) {
-      return
-    }
-    const { data } = await axios.post<Task>('/tasks', { title })
-    setTasks([data].concat(tasks))
-  }
-  const updateTask = async (task: Task, params: TaskUpdateParams) => {
-    if (!params.title) {
-      return
-    }
-    const { data } = await axios.patch<Task>(`/tasks/${task.id}`, params)
-    setTasks(tasks.map(t => {
-      if (t.id === data.id) {
-        return data
-      }
-      return t
-    }).sort(compareTasks))
-  }
-  const toggleTaskCompleted = async (task: Task) => {
-    const { data } = await axios.patch<Task>(`/tasks/${task.id}`, {
-      completed: !task.completed
-    })
-    if (data.completed) {
-      setTasks(tasks.filter(t => t.id !== data.id))
-      setCompletedTasks(completedTasks.concat(data).sort(compareTasks))
-    } else {
-      setCompletedTasks(completedTasks.filter(t => t.id !== data.id))
-      setTasks(tasks.concat(data).sort(compareTasks))
-    }
-  }
-  const deleteTask = async (task: Task) => {
-    await axios.delete(`/tasks/${task.id}`)
-    setTasks(tasks.filter(t => t.id !== task.id))
-    setCompletedTasks(completedTasks.filter(t => t.id !== task.id))
-  }
-  
-  useEffect(() => {
-    axios.get<Task[]>('/tasks')
-      .then(({ data }) => {
-        setTasks(data)
-      })
-    axios.get<Task[]>('/tasks/completed')
-      .then(({ data }) => {
-        setCompletedTasks(data)
-      })
-  }, [])
+  const {
+    state: { tasks, completedTasks },
+    addTask,
+    toggleTaskCompleted,
+    updateTask,
+    deleteTask
+  } = useTodo()
 
   return (
     <div>
---
+++ spa/src/hooks/useTodo.ts
@@ -0,0 +1,106 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function compareTasks(a: Task, b: Task): number {
+  return Date.parse(b.updatedAt) - Date.parse(a.updatedAt)
+}
+
+type State = {
+  tasks: Task[],
+  completedTasks: Task[]
+}
+
+export default function useTodo() {
+  const [state, setState] = useState<State>({
+    tasks: [],
+    completedTasks: []
+  })
+  
+  useEffect(() => {
+    (async () => {
+      const [tasks, completedTasks] = await Promise.all([
+        new Promise<Task[]>(resolve => {
+          axios.get<Task[]>('/tasks')
+            .then(({ data }) => {
+              resolve(data)
+            })
+        }),
+        new Promise<Task[]>(resolve => {
+          axios.get<Task[]>('/tasks/completed')
+            .then(({ data }) => {
+              resolve(data)
+            })
+        })
+      ])
+      setState({
+        ...state,
+        tasks,
+        completedTasks
+      })
+    })
+    // eslint-disable-next-line
+  }, [])
+
+  const addTask = async (title: string) => {
+    if (!title) {
+      return
+    }
+    const { data } = await axios.post<Task>('/tasks', { title })
+    setState({
+      ...state,
+      tasks: [data].concat(state.tasks)
+    })
+  }
+
+  const updateTask = async (task: Task, params: TaskUpdateParams) => {
+    if (!params.title) {
+      return
+    }
+    const { data } = await axios.patch<Task>(`/tasks/${task.id}`, params)
+    setState({
+      ...state,
+      tasks: state.tasks.map(t => {
+        if (t.id === data.id) {
+          return data
+        }
+        return t
+      }).sort(compareTasks)
+    })
+  }
+
+  const toggleTaskCompleted = async (task: Task) => {
+    const { data } = await axios.patch<Task>(`/tasks/${task.id}`, {
+      completed: !task.completed
+    })
+    if (data.completed) {
+      setState({
+        ...state,
+        tasks: state.tasks.filter(t => t.id !== data.id),
+        completedTasks: state.completedTasks.concat(data).sort(compareTasks)
+      })
+    } else {
+      setState({
+        ...state,
+        completedTasks: state.completedTasks.filter(t => t.id !== data.id),
+        tasks: state.tasks.concat(data).sort(compareTasks)
+      })
+    }
+  }
+
+  const deleteTask = async (task: Task) => {
+    await axios.delete(`/tasks/${task.id}`)
+    setState({
+      ...state,
+      tasks: state.tasks.filter(t => t.id !== task.id),
+      completedTasks: state.completedTasks.filter(t => t.id !== task.id)
+    })
+  }
+
+  return {
+    state,
+    addTask,
+    updateTask,
+    toggleTaskCompleted,
+    deleteTask
+  }
+}
